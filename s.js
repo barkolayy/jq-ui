6d4d5aff5a14108430d04b42d61ec8d2dbae2d32
DEVIL <Andreyxxl7@yandex.ru>
2022-03-14 12:39:58 +0700

    2.1

--- s.js
+++ s.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-	$("div").draggable({axis:"x", cursor:"move"});
-	$("h2").draggable({axis:"y", cursor:"move"});
+$(document).ready(function(){
+
+   $("#accordion1").accordion();
 
 });
