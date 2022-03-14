89b7e226dd68be64ae1db3eeeef00075e8e9ee1a
DEVIL <Andreyxxl7@yandex.ru>
2022-03-14 12:04:12 +0700

    fff

--- s.js
+++ s.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
-	$("div").draggable({axis:"x", cursor:"move"});
-	$("h2").draggable({axis:"y", cursor:"move"});
-
+	$("#el1").draggable();
+	$("#el1").resizable();
+	$("#el2").draggable({stack:"#drop1"});
+	$("#el3").draggable({stack:"#drop1"});
+	$("#sort1").sortable();
+	$("#drop1").droppable({
+		drop:function(){	
+		$("#drop1").css("background-color", "#22b348");
+		$("#el2").css("display", "none")
+		}
+	});
+	$("#drop2").droppable({
+		accept: "#el3",
+		drop: function(){
+		$("#drop2").css("background-color", "#22b348");
+		$("#el3").css("display", "none")
+		}
+	})
 });
